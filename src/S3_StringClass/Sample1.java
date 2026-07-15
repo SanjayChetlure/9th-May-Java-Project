d2a5d4fcd704737d375ae520dc580c6d8a17cc4e
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-07-15 20:29:43 +0530

    15th July 2026

--- src/S3_StringClass/Sample1.java
+++ src/S3_StringClass/Sample1.java
@@ -1,10 +1,10 @@
-package S3_StringClass;
-
-public class Sample1 extends String
-{
-    public static void main(String[] args)
-    {
-        String s1="abcd dldjd dkldwnd lddwjqd ldldwdn ";
-
-    }
-}
+//package S3_StringClass;
+//
+//public class Sample1 extends String
+//{
+//    public static void main(String[] args)
+//    {
+//        String s1="abcd dldjd dkldwnd lddwjqd ldldwdn ";
+//
+//    }
+//}
