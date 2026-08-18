1b063f20293e4c4464788bbfbf10e76e6e73f61a
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-08-18 20:00:54 +0530

    18th Aug 2026

--- src/S3_Collection/Ex3_LinkedList.java
+++ src/S3_Collection/Ex3_LinkedList.java
@@ -1,5 +1,8 @@
 package S3_Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Ex3_LinkedList
 {
     public static void main(String[] args)
@@ -18,6 +21,50 @@ public class Ex3_LinkedList
         System.out.println(ll.isEmpty());
         System.out.println(ll.contains("ramesh"));
         System.out.println(ll.get(3));
+
+        //update data
+        ll.set(0,"RAMESH");
+        System.out.println(ll);
+
+        //add data in between linkedlist
+        ll.add(4,"abc");
+        System.out.println(ll);
+
+        //remove data in between linkedlist
+        ll.remove(4);
+        System.out.println(ll);
+
+        System.out.println("------print data using for loop---------");
+        for(int i=0; i<=ll.size()-1; i++)
+        {
+            System.out.println(ll.get(i));
+        }
+
+        System.out.println("-----print data using for each loop------");
+        for(Object s1:ll)
+        {
+            System.out.println(s1);
+        }
+
+        System.out.println("-----print data using Iterator cursor------");
+        Iterator itr = ll.iterator();
+        while (itr.hasNext())
+        {
+            System.out.println(itr.next());
+        }
+
+        System.out.println("-----print data using ListIterator cursor------");
+        ListIterator listItr = ll.listIterator();
+        while (listItr.hasNext())
+        {
+            System.out.println(listItr.next());
+        }
+
+        System.out.println("----");
+        ll.clear();
+        System.out.println(ll.size());
+
+
     }
 
 }
